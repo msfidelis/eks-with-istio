322b4d025d26d1496fb6ea0802c086023e0e6d97
Matheus Fidelis <matheus@nanoshots.com.br>
2023-03-23 23:18:17 -0300

    argo observability

--- helm_argo_rollouts.tf
+++ helm_argo_rollouts.tf
@@ -12,6 +12,28 @@ resource "helm_release" "argo_rollouts" {
         value = true
     }
 
+
+    set {
+        name = "controller.metrics.enabled"
+        value = true
+    }
+
+    # set {
+    #     name = "podAnnotations.prometheus\\.io/scrape"
+    #     value = true
+    # }
+
+    # set {
+    #     name = "podAnnotations.prometheus\\.io/path"
+    #     value = "/metrics"
+    # }
+
+    #     set {
+    #     name = "podAnnotations.prometheus\\.io/port"
+    #     value = "8090"
+    # }
+
+
     depends_on = [
         aws_eks_cluster.eks_cluster,
         aws_eks_node_group.cluster,
