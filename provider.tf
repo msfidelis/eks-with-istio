a3dcc4bcc123610357e2b506d38a1b93f252539c
Matheus Fidelis <matheus@nanoshots.com.br>
2023-04-10 15:53:48 -0300

    Update EKS 1.24

--- provider.tf
+++ provider.tf
@@ -11,11 +11,15 @@ terraform {
     kubernetes = {
       source  = "kubernetes"
       version = "~> 2.0"
-    }   
+    } 
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }  
     tls = {
       source  = "tls"
       version = "~> 3.1.0"
-    }      
+    }
   }
 }
 
@@ -38,4 +42,10 @@ provider "kubernetes" {
   host                   =  aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate =  base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  =  data.aws_eks_cluster_auth.default.token 
+}
+
+provider "kubectl" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.default.token
 }
