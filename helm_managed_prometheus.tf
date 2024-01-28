bbdc85a2ceb32aa0c87ac66f74f6316d289153bb
Matheus Fidelis <matheus@nanoshots.com.br>
2024-01-28 17:35:49 -0300

    fix(karpenter): dependency

--- helm_managed_prometheus.tf
+++ helm_managed_prometheus.tf
@@ -1,69 +1,101 @@
+# resource "helm_release" "managed_prometheus" {
+
+#   count = var.enable_managed_prometheus ? 1 : 0
+
+#   name             = "prometheus"
+#   chart            = "prometheus"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   namespace        = "prometheus"
+#   create_namespace = true
+
+#   set {
+#     name  = "serviceAccounts.server.name"
+#     value = "managed-prometheus"
+#   }
+
+#   set {
+#     name  = "serviceAccounts.server.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.managed_prometheus_role.arn
+#   }
+
+#   set {
+#     name  = "server.remoteWrite[0].url"
+#     value = format("%sapi/v1/remote_write", aws_prometheus_workspace.main[0].prometheus_endpoint)
+#   }
+
+#   set {
+#     name  = "server.remoteWrite[0].sigv4.region"
+#     value = var.aws_region
+#   }
+
+#   set {
+#     name  = "server.remoteWrite[0].queue_config.max_samples_per_send"
+#     value = "1000"
+#   }
+
+#   set {
+#     name  = "server.remoteWrite[0].queue_config.max_shards"
+#     value = "200"
+#   }
+
+#   set {
+#     name  = "server.remoteWrite[0].queue_config.capacity"
+#     value = "2500"
+#   }
+
+#   set {
+#     name  = "server.persistentVolume.enabled"
+#     value = "false"
+#   }
+
+
+#   set {
+#     name  = "prometheus-pushgateway.enabled"
+#     value = "false"
+#   }
+
+
+#   set {
+#     name  = "prometheus-pushgateway.enabled"
+#     value = "false"
+#   }
+
+#   set {
+#     name  = "alertmanager.enabled"
+#     value = "false"
+#   }
+
+#   depends_on = [
+#     aws_eks_cluster.eks_cluster,
+#     aws_eks_node_group.cluster,
+#     kubernetes_config_map.aws-auth
+#   ]
+# }
+
+
+
+
 resource "helm_release" "managed_prometheus" {
 
   count = var.enable_managed_prometheus ? 1 : 0
 
   name             = "prometheus"
-  chart            = "prometheus"
+  chart            = "kube-prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
   namespace        = "prometheus"
   create_namespace = true
 
-  set {
-    name  = "serviceAccounts.server.name"
-    value = "managed-prometheus"
-  }
+  version = "45.8.0"
 
-  set {
-    name  = "serviceAccounts.server.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.managed_prometheus_role.arn
-  }
+  # set {
+  #   name  = "fullnameOverride"
+  #   value = "prometheus"
+  # }
 
-  set {
-    name  = "server.remoteWrite[0].url"
-    value = format("%sapi/v1/remote_write", aws_prometheus_workspace.main[0].prometheus_endpoint)
-  }
+  values = [
+    "${file("./helm/prometheus/values.yml")}"
+  ]
 
-  set {
-    name  = "server.remoteWrite[0].sigv4.region"
-    value = var.aws_region
-  }
-
-  set {
-    name  = "server.remoteWrite[0].queue_config.max_samples_per_send"
-    value = "1000"
-  }
-
-  set {
-    name  = "server.remoteWrite[0].queue_config.max_shards"
-    value = "200"
-  }
-
-  set {
-    name  = "server.remoteWrite[0].queue_config.capacity"
-    value = "2500"
-  }
-
-  set {
-    name  = "server.persistentVolume.enabled"
-    value = "false"
-  }
-
-
-  set {
-    name  = "prometheus-pushgateway.enabled"
-    value = "false"
-  }
-
-
-  set {
-    name  = "prometheus-pushgateway.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "alertmanager.enabled"
-    value = "false"
-  }
 
   depends_on = [
     aws_eks_cluster.eks_cluster,
@@ -71,4 +103,3 @@ resource "helm_release" "managed_prometheus" {
     kubernetes_config_map.aws-auth
   ]
 }
-
